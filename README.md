912591cf439e34b2a3dbf28c05b215bd2b44fd39
hangry <quentin.sarazi@gmail.com>
2020-09-04 12:51:02 +0200

    Hello world!

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # test_4d
+
+Compilé via CMake avec une toolchain MinGW-w64 v3.3 (gcc 4.8.3) sous Windows
+
+
---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by qjps4 on 9/4/2020.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int, char **) {
+    cout << "Hello World!" << endl;
+    return 0;
+}
