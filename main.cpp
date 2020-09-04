cf7f0aa8eb9f7bc937601bd9c3653ac58010cdba
hangry <quentin.sarazi@gmail.com>
2020-09-04 16:44:04 +0200

    fini

--- main.cpp
+++ main.cpp
@@ -38,7 +38,7 @@ vector<Token> StringToToken(const std::string &text) {
 }
 
 int main(int ac, char **av) {
-    if (ac != 3) {
+    if (ac != 2) {
         cout << "Usage: " << av[0] << " dict_file" << endl;
         return 1;
     }
@@ -47,9 +47,12 @@ int main(int ac, char **av) {
         string text;
         vector<string> suggestions{10};
         Dictionary dict{string(av[1])};
-        ifstream in{av[2]};
+        ofstream out{"fautes.json"};
 
-        while (getline(in, text)) {
+        out << "[" << endl;
+        bool first = true;
+        size_t start = 0;
+        while (getline(cin, text)) {
             vector<Token> tokens = StringToToken(text);
             for (auto &&t : tokens) {
 
@@ -57,24 +60,45 @@ int main(int ac, char **av) {
                 if (all_of(t.word.begin(), t.word.end(), [](char c) {return isupper(c);}))
                     continue;
 
-                cout << t.word << endl;
-                if (!dict.Verify(t.word, &suggestions)) {
-                    for (auto &&s : suggestions) {
-                        cout << "  " << s << endl;
-                    }
-                    //check first caps
-                    //add to json
+
+                if (dict.Verify(t.word, &suggestions))
+                    continue;
+
+                bool has_first_upper = isupper(t.word[0]);
+                if (first)
+                    first = false;
+                else
+                    out << ",";
+                out << "{\n\"word\": \"" << t.word << "\",\n"
+                    << "\"beginOffset\": " << t.start << ",\n"
+                    << "\"endOffset\": " << t.end << ",\n"
+                    << "\"suggestions\": [";
+                bool first_word = true;
+                for (auto s : suggestions) {
+                    if (first_word)
+                        first_word = false;
+                    else
+                        out << ", ";
+                    if (has_first_upper)
+                        s[0] = toupper(s[0]);
+                    out << "\"" << s << "\"";
                 }
-                cout << endl;
+                out << "]\n}\n";
+
+                //write correct text
+                cout << text.substr(start, t.start - start);
+                if (has_first_upper)
+                    suggestions[0][0] = toupper(suggestions[0][0]);
+                cout << suggestions[0];
+                start = t.end;
             }
         }
+        out << "]" << endl;
 
     } catch (const exception &e) {
         cout << e.what() << endl;
         return 2;
     }
 
-    //use json to correct
-    //print
     return 0;
 }
